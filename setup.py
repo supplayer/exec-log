7bd0571803126176fc51afc113681029fdb7fb88
xingbo <xingbo@mozat.com>
2020-12-03 08:02:38 +0800

    fix import problem

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/supplayer/loguru-notification",
-    packages=setuptools.find_packages(exclude=('tests',)),
+    packages=setuptools.find_packages(exclude=('tests', 'requirements.txt')),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
