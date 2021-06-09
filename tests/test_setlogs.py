5eb8a069baf192dfa39bc97e6c8d1b655df437b0
supplayer <x254724521@hotmail.com>
2021-06-09 18:22:43 +0800

    add workflows and change README and tests

--- tests/test_setlogs.py
+++ tests/test_setlogs.py
@@ -2,11 +2,9 @@ from configlog.setlogs import Logger
 
 
 logger = Logger(
-    dsn='Sentry dsn URL',  # Sentry dsn URL
     webhook_url='Slack webhook URL',  # Slack webhook URL  # noqa
     es_host='log on ElasticSearch host ip and port',  # log on ElasticSearch host ip and port
     type_on=False,
-    # channel='mozat'
 )
 
 
